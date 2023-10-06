c96ea2c5ffb83230d1069d2916982669d76969a0
Olga Vasilevskaya <olgavasilevskaya@MacBook-Air-Olga.local>
2023-10-06 16:03:17 +0300

    added navbar

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components";
+import { Header, Navbar } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
       <Header />
+      <Navbar />
       <nav>
         <span className="text-4xl text-secondary">Comfy</span>
       </nav>
