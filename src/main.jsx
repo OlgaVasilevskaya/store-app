8f6facd21603e863d78e8bc9a257d72ea4367929
Olga Vasilevskaya <olgavasilevskaya@MacBook-Air-Olga.local>
2023-10-30 20:40:06 +0300

    added single product page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+
+import App from './App.jsx';
+
+import { store } from './store/index.jsx';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+    <ToastContainer position='top-center' />
+  </Provider>
 )
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import { FeaturedProducts, Hero } from "../components";
+
 import { customFetch } from "../utils";
 
 const url = '/products?featured=true';
